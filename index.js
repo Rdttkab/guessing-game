7fb3f70b157bc6058d47c1ff857639f81051d24e
Rediet Tekola <43878585+Rdttkab@users.noreply.github.com>
2022-10-20 19:34:59 +0000

    add range as fun parameter

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-function generateRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min)
+function generateRandomNumber(min, max, range) {
+  return Math.floor(Math.random() * ((max + range) - min + 1) + min)
 }
 let totalPoint = 0
 let gameContinue = true
@@ -9,15 +9,15 @@ let max = 2
 const username = prompt('Enter your username: ')
 
 while (gameContinue) {
-  let num = generateRandomNumber(min, max)
+  let num = generateRandomNumber(min, max, range)
   // console.log(num)
   console.log('Stage', range + 1)
-  let gussedNumber = parseInt(prompt(`Enter a number between ${min} and ${max} : `))
+  let gussedNumber = parseInt(prompt(`Enter a number between ${min} and ${max + range} : `))
+  
   if (num === gussedNumber) {
+    console.log(`${username} You gussed Right, Your total point is ${totalPoint}`)
     totalPoint += 1
     range += 1
-    max += 1
-    console.log(`${username} You gussed Right, Your total point is ${totalPoint}`)
   } else {
     console.log(`${username} You gussed Wrong, Your total point is ${totalPoint}`)
     gameContinue = false
